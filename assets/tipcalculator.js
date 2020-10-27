bf2defbb2a5e416c5f8cfc7320f97f0a8eb8a4ed
RG-Mac <ramong5406@gmail.com>
2020-10-27 16:17:16 -0500

    created calculateTip function

--- assets/tipcalculator.js
+++ assets/tipcalculator.js
@@ -1,4 +1,9 @@
-
+const calculateTip = () => {
+    let billAmount = document.getElementById("billAmt".value);
+    let serviceQual = "";
+    document.getElementById("serviceQual").value;
+    let numberOfPeople = document.getElementById("peopleAmt")
+}
 
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";

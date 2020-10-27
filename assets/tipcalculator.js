1da2c27debdda644af786885cdd398b4ca04ebe0
RG-Mac <ramong5406@gmail.com>
2020-10-27 15:47:16 -0500

    created click event and hiding tip amount

---
+++ assets/tipcalculator.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+}

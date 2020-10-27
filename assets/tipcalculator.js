2a95ffc93aea7029b1de1e898d51ff8d6b08b953
RG-Mac <ramong5406@gmail.com>
2020-10-27 17:38:22 -0500

    fixed NaN issue, changed color scheme

--- assets/tipcalculator.js
+++ assets/tipcalculator.js
@@ -1,14 +1,17 @@
 
+//function to calulate tip amount
+function calculateTip() {
+    let billAmount = document.getElementById("billAmt").value;
+    let serviceQual = document.getElementById("serviceQual").value;
+    let numberOfPeople = document.getElementById("peopleAmt").value;
 
-const calculateTip = () => {
-    if (billAmount === "" || serviceQual === 0) {
-    var billAmount = document.getElementById("billAmt".value);
-    var serviceQual = document.getElementById("serviceQual").value;
-    var numberOfPeople = document.getElementById("peopleAmt").value;
+    //Checks if the Bill Amount and Service Quality areas have a value entered
+    if (billAmount === " " || serviceQual === 0) {
         alert("A value must be entered");
         return;
     }
 
+    //Checks if there is a value inside Number of People or if the value is greater than 0
     if (numberOfPeople === "" || numberOfPeople <= 1) {
         numberOfPeople = 1;
         document.getElementById("each").style.display = "none";
@@ -16,19 +19,23 @@ const calculateTip = () => {
         document.getElementById("each").style.display = "block";
     }
 
-    let total = (billAmount * serviceQual) / numberOfPeople;
+    //Calculates Tip
+    var total = (billAmount * serviceQual) / numberOfPeople;
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2);
 
+    //renders total 
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
 
 
 }
 
+//hides area for total until click event happens
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
+//click event to begin Tip Calculation
 document.getElementById("calculate").onclick = function() {
     calculateTip();
 }

41c2388dd61538234b33b9a27089b8c94a421dfc
RG-Mac <ramong5406@gmail.com>
2020-10-27 16:30:37 -0500

    checking if inputs or empty, less than or greater than 1

--- assets/tipcalculator.js
+++ assets/tipcalculator.js
@@ -1,8 +1,22 @@
 const calculateTip = () => {
     let billAmount = document.getElementById("billAmt".value);
-    let serviceQual = "";
+    // let serviceQual = "";
     document.getElementById("serviceQual").value;
-    let numberOfPeople = document.getElementById("peopleAmt")
+    let numberOfPeople = document.getElementById("peopleAmt").value;
+
+    if (billAmount === "" || serviceQual === 0) {
+        alert("A value must be entered");
+        return;
+    }
+
+    if (numberOfPeople === "" || numberOfPeople <= 1) {
+        numberOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    
 }
 
 document.getElementById("totalTip").style.display = "none";
